a30e6eae8736b103c95b2e37888613a9ee06c879
Atif Aiman <alserembani94@gmail.com>
2021-07-04 15:52:17 +0800

    Fixing duplicate id on social media icons

--- components/Main/PUM.tsx
+++ components/Main/PUM.tsx
@@ -36,43 +36,43 @@ const PUM: React.FC<PUMProps> = ({
       <div className={styles.cta}>
         
         <Link href="/twitter">
-          <a target="_blank" id="twitter" className={styles.twitter} aria-label="My Twitter">
+          <a target="_blank" id="footer_twitter" className={styles.twitter} aria-label="My Twitter">
               <IoLogoTwitter size={socmedSize} />
           </a>
         </Link>
         
         <Link href="/facebook">
-          <a target="_blank" id="facebook" className={styles.facebook} aria-label="My Facebook">
+          <a target="_blank" id="footer_facebook" className={styles.facebook} aria-label="My Facebook">
               <IoLogoFacebook size={socmedSize} />
           </a>
         </Link>
         
         <Link href="/behance">
-          <a target="_blank" id="behance" className={styles.behance} aria-label="My Behance">
+          <a target="_blank" id="footer_behance" className={styles.behance} aria-label="My Behance">
               <IoLogoBehance size={socmedSize} />
           </a>
         </Link>
         
         <Link href="/github">
-          <a target="_blank" id="github" className={styles.github} aria-label="My Github">
+          <a target="_blank" id="footer_github" className={styles.github} aria-label="My Github">
               <IoLogoGithub size={socmedSize} />
           </a>
         </Link>
         
         <Link href="/linkedin">
-          <a target="_blank" id="linkedin" className={styles.linkedin} aria-label="My LinkedIn">
+          <a target="_blank" id="footer_linkedin" className={styles.linkedin} aria-label="My LinkedIn">
               <IoLogoLinkedin size={socmedSize} />
           </a>
         </Link>
                     
         <Link href="/medium">
-          <a target="_blank" id="medium" className={styles.medium} aria-label="My Medium Articles">
+          <a target="_blank" id="footer_medium" className={styles.medium} aria-label="My Medium Articles">
             <IoLogoMedium size={socmedSize} />
           </a>
         </Link>
         
         <Link href="/devto">
-          <a target="_blank" id="devto" className={styles.devto} aria-label="My Dev Dot To Article">
+          <a target="_blank" id="footer_devto" className={styles.devto} aria-label="My Dev Dot To Article">
               <FaDev size={socmedSize} />
           </a>
         </Link>
