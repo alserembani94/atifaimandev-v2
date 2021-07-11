599465005640bbe09d36fb57fa642fbfba0fc57e
Atif Aiman <alserembani94@gmail.com>
2021-07-12 05:45:57 +0800

    🔐 Modifying CSP information

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const securityHeaders = [
-    {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'on',
-    },
-    {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block',
-    },
-    {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-    },
+    // {
+    //     key: 'X-DNS-Prefetch-Control',
+    //     value: 'on',
+    // },
+    // {
+    //     key: 'X-XSS-Protection',
+    //     value: '1; mode=block',
+    // },
+    // {
+    //     key: 'X-Content-Type-Options',
+    //     value: 'nosniff',
+    // },
     // {
     //     key: 'Content-Security-Policy',
     //     value: `default-src 'self';`,
